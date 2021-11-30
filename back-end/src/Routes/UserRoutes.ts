64278c844004a23e3f4d82c2ae89159cfa530349
Gabriel-Andrade36 <gabrielndcorrea@gmail.com>
2021-11-30 08:26:37 -0300

    fix(routes) routes fixed

---
+++ back-end/src/Routes/UserRoutes.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.post("/signin", (req, res){
+res.send("hakunamatata")
+})
+
+app.post("/signouut", (req, res){
+res.send("hakunamatata")
+})
+
+app.post("/signup", (req, res){
+res.send("hakunamatata")
+})
+
