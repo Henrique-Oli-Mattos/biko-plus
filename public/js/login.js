9007087ff5801d1dd47139d281fc02f809c2aaa5
Gabriel Andrade Correa <gabrielndcorrea@gmail.com>
2021-12-04 08:28:01 -0300

    hosting

--- public/js/login.js
+++ public/js/login.js
@@ -20,13 +20,6 @@ alert("Login works");
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyB4rVAcVULCUb2AlUecXT89edZzcaD4SUw",
-  authDomain: "biko-plus-project-1e054.firebaseapp.com",
-  databaseURL: "https://biko-plus-project-1e054-default-rtdb.firebaseio.com",
-  projectId: "biko-plus-project-1e054",
-  storageBucket: "biko-plus-project-1e054.appspot.com",
-  messagingSenderId: "870890276247",
-  appId: "1:870890276247:web:a6ccbdff7f0b9f9d6670d2",
   //enquanto n tiver enviroment vai ficar vazio no github
 };
 const app = initializeApp(firebaseConfig);
--- public/js/worker.js
+++ public/js/worker.js
@@ -16,45 +16,65 @@ import {
 //     console.log("No User");
 //   }
 // });
-
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyB4rVAcVULCUb2AlUecXT89edZzcaD4SUw",
-  authDomain: "biko-plus-project-1e054.firebaseapp.com",
-  databaseURL: "https://biko-plus-project-1e054-default-rtdb.firebaseio.com",
-  projectId: "biko-plus-project-1e054",
-  storageBucket: "biko-plus-project-1e054.appspot.com",
-  messagingSenderId: "870890276247",
-  appId: "1:870890276247:web:a6ccbdff7f0b9f9d6670d2",
   //enquanto n tiver enviroment vai ficar vazio no github
 };
 
 const app = initializeApp(firebaseConfig);
 
-document.getElementById("btnSalvar").addEventListener("click", () => {
+alert("working");
+
+document.getElementById("btnSalvarAU").addEventListener("click", () => {
   register();
+  //   alert("CUUUUUUU");
 });
 
 //Configura a função cadastrar
 function register() {
   alert("Register");
   //pegar todos os inputs, no momento apenas do usuário.
-  let name = document.getElementById("nomeCadastro").value;
-  let email = document.getElementById("emailCadastro").value;
-  let password = document.getElementById("senhaCadastro").value;
-  let data = document.getElementById("dataCadastro").value;
-  let checkbox = document.getElementById("termoUsoCadastro").value;
+  let nome = document.getElementById("nomeAU").value;
+  let data = document.getElementById("dataAU").value;
+  let email = document.getElementById("emailAU").value;
+  let senha = document.getElementById("senhaAU").value;
+  let profissao = document.getElementById("profissaoAU").value;
+  let tempoexp = document.getElementById("tempoexpAU").value;
+  let telefone = document.getElementById("telefoneAU").value;
+  let cpf = document.getElementById("cpfAU").value;
+  let curso = document.getElementById("cursoAU").value;
+  let instituição = document.getElementById("instituiçãoAU").value;
+  let btnSalvar = document.getElementById("btnSalvarAU").value;
 
   //   let name = document.getElementById("nome").value;
   //   let email = document.getElementById("email").value;
   //   let password = document.getElementById("senha").value;
 
-  console.log(name, email, password, data);
+  console.log(
+    nome,
+    data,
+    email,
+    senha,
+    profissao,
+    tempoexp,
+    telefone,
+    cpf,
+    curso,
+    instituição,
+    btnSalvar
+  );
   if (
-    !validate_field(email) ||
-    !validate_field(name) ||
-    !validate_field(password) ||
-    !validate_field(data)
+    !validate_field("nome") ||
+    !validate_field("data") ||
+    !validate_field("email") ||
+    !validate_field("senha") ||
+    !validate_field("profissao") ||
+    !validate_field("tempoexp") ||
+    !validate_field("telefone") ||
+    !validate_field("cpf") ||
+    !validate_field("curso") ||
+    !validate_field("instituição") ||
+    !validate_field("btnSalvar")
   ) {
     alert("Um ou mais campos vazios");
     return;
@@ -64,31 +84,47 @@ function register() {
     alert("email inválido");
     return;
   }
-  if (!validate_password(password)) {
+  if (!validate_password(senha)) {
     alert("senha inválida");
     return;
   }
 
+  // if (!checkbox) {
+  //   alert("checkbox");
+  // }
+
   const auth = getAuth(app);
   const db = getDatabase(app);
 
-  createUserWithEmailAndPassword(auth, email, password)
+  createUserWithEmailAndPassword(auth, email, senha)
     .then((userCrential) => {
       let user = userCrential.user;
       console.log(user);
       console.log(userCrential);
 
       // //Adiciona este usuário ao banco de dados
+
       set(ref(db, "users/" + user.uid), {
-        name: name,
+        name: nome,
         email: email,
         data: data,
-        isWorker: false,
+        isWorker: true,
         uid: user.uid,
         last_login: Date.now(),
       });
 
-      alert("User Created");
+      set(ref(db, "workersInfo/" + user.uid), {
+        profissao: profissao,
+        tempoexp: tempoexp,
+        telefone: telefone,
+        cpf: cpf,
+        curso: curso,
+        instituição: instituição,
+        uid: user.uid,
+        last_login: Date.now(),
+      });
+
+      alert("worker Created");
     })
     .catch((error) => {
       let error_message = error.message;
